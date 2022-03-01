f8fc0d7cf87f498a2cb7aae812b2b81fcf44ed82
Hannes Preishuber <hannesp@ppedv.de>
2022-03-01 11:33:50 +0100

    Routen Tabelle und Beispiel

---
+++ BlazorTraining22/Pages/modul02/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorTraining22.Pages.modul02
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

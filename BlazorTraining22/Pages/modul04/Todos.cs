599d224317f7f0bb18a5a7cfe65bec9ade85012a
Hannes Preishuber <hannesp@ppedv.de>
2022-03-01 16:56:15 +0100

    Virtualisierte lange Liste

---
+++ BlazorTraining22/Pages/modul04/Todos.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorTraining22.Pages.modul04
+{
+
+    
+
+    public class Todos
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+
+}

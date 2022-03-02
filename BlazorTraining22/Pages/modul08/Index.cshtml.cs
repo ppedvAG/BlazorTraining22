b8cf9ad1715f586f49ede5b5442ebc719c25b2dd
Hannes Preishuber <hannesp@ppedv.de>
2022-03-02 15:59:03 +0100

    JavaScript Schnippsel

---
+++ BlazorTraining22/Pages/modul08/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorTraining22.Pages.modul08
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

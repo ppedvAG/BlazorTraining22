36508aed9c50b2f64569b8b3760a8fbb65203196
Hannes Preishuber <hannesp@ppedv.de>
2022-03-02 10:36:01 +0100

    Formular valdierung mit Model und annotationen

---
+++ BlazorTraining22/Pages/modul05/Person.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTraining22.Pages.modul05
+{
+    public class Person
+    {
+        public int Id { get; set; } 
+        [Required(ErrorMessage ="da muss was rein")]
+        [StringLength(10,ErrorMessage ="Max 10")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        [Range(130,195,ErrorMessage ="Größe passt nicht")]
+        public int Gross { get; set; }
+
+        [Required(ErrorMessage = "da muss was rein")]
+        public DateTime GebDat { get; set; }
+
+    }
+}

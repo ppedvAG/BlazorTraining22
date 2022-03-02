7ef3f55aea5971013f6cb534cf117b7348019050
Hannes Preishuber <hannesp@ppedv.de>
2022-03-02 10:07:03 +0100

    HTML Formular Grundlagen

---
+++ BlazorTraining22/Pages/modul05/formular.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorTraining22.Pages.modul05
+{
+    public class formularModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
